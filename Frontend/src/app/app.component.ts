61c6a008d093bd953710d78ca6bff14c6c7a6281
ganjah454 <torres_di_da@hotmail.com>
2021-05-18 23:21:12 -0300

    Concepto ngSwitch

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -9,8 +9,10 @@ export class AppComponent {
   
  listadoEstudiantes: any[]=[
    {nombre: 'Juan', Estado:'Regular'},
+   {nombre: 'Ana', Estado:'Libre'},
    {nombre: 'Miguel', Estado:'Regular'},
-   {nombre: 'Mario', Estado:'Promocionado'}
+   {nombre: 'Mario', Estado:'Promocionado'},
+   {nombre: 'Nicolas', Estado:'Libre'}
  ]
 
 mostrar = true;
