83dc4c5ba67d420c174e361aa9d92be3f7e5d93d
ganjah454 <torres_di_da@hotmail.com>
2021-07-01 11:59:51 -0300

    Se agrego el modulo de registro con sus respectivas validacion y una validacion personalizada

--- Frontend/src/app/components/inicio/login/login.component.ts
+++ Frontend/src/app/components/inicio/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {Usuario} from '../../../models/usuario';
 
 
 @Component({
@@ -25,6 +26,16 @@ export class LoginComponent implements OnInit {
 
   log(): void {
     console.log(this.login); 
+
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
+    }
+
+    console.log(usuario);
   }
+
+
+
 
 }
--- Frontend/src/app/components/inicio/register/register.component.ts
+++ Frontend/src/app/components/inicio/register/register.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  register : FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.register = this.fb.group({
+      usuario: ['', Validators.required],
+      clave: ['', [Validators.required, Validators.minLength(4)]],
+      nuevaClave: ['', [Validators.required]]
+     });
+
+  }
 
   ngOnInit(): void {
   }
+
+
+  registrarUsuarios(): void {
+    console.log(this.register);
+  }
+
+ 
 
 }
