5575b4c252f88695e67b1b1e99eb9d276f94ba88
ganjah454 <torres_di_da@hotmail.com>
2021-09-05 17:08:03 -0300

    ae agrego toastr que no sale con formato y los spinner en el modulo login

--- Frontend/src/app/components/inicio/login/login.component.ts
+++ Frontend/src/app/components/inicio/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {Usuario} from '../../../models/usuario';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -10,9 +12,13 @@ import {Usuario} from '../../../models/usuario';
 })
 export class LoginComponent implements OnInit {
 
+  loading = false;
   login: FormGroup;
+  
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private toastr: ToastrService,
+              private router: Router) {
     this.login = this.fb.group({
 
       usuario: ['', Validators.required],
@@ -32,6 +38,19 @@ export class LoginComponent implements OnInit {
       password: this.login.value.password
     }
 
+    this.loading = true;
+    setTimeout(()=>{     
+      if (usuario.nombreUsuario === 'diegot' && usuario.password === 'admin123')
+    {
+        this.router.navigate(['/dashboard']);
+        this.login.reset();
+    }else{
+      this.toastr.error('Se ingreso un usuario incorrecto', 'Error');
+      this.login.reset();
+    }
+    this.loading = false;
+    },3000)
+    
     console.log(usuario);
   }
 
